7d09efe9660c970e92929784433dcf7a7d8d6e26
Claudius Ma <heiclaudius@gmail.com>
2022-09-19 14:34:48 -0400

    lovefromSJ

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import "./font/cheltenham_bt.ttf"
 import "./font/OPTIEngraversOldEnglish.otf"
+import './font/LoveFromSJA-Bold.ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
